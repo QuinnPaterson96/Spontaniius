b3bc757e1756abc0d57015e08b65d2f72c5dfacb
russell-waterhouse <Russell.L.Waterhouse@gmail.com>
2020-02-21 15:33:23 -0800

    created CreateEventActivity.kt and CreateEventFragment.kt

---
+++ app/src/main/java/com/example/spontaniius/ui/create_event/CreateEventActivity.kt
@@ -0,0 +1,13 @@
+package com.example.spontaniius.ui.create_event
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.spontaniius.R
+
+class CreateEventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_event)
+    }
+}

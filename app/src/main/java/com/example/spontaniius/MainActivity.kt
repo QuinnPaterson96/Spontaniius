b0bf47ebf675d52bc545d1d194938c33726b8689
Cord Corcese <badwithnames14@gmail.com>
2020-02-21 17:32:32 -0800

    Made UwU button work and open a new screen

--- app/src/main/java/com/example/spontaniius/MainActivity.kt
+++ app/src/main/java/com/example/spontaniius/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.spontaniius
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button1: Button = findViewById(R.id.UwU)
+        button1.setOnClickListener {
+            // Handler code here.
+            val intent = Intent(this, PromotionTab::class.java)
+            startActivity(intent);
+        }
     }
+
+
 }
+

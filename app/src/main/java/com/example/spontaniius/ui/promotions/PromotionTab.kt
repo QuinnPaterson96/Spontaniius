60b3b9143934014ca646ac4b22114f5af6506f13
Cord Corcese <badwithnames14@gmail.com>
2020-07-06 12:39:49 -0700

    Finally figured out how string passing works lol

--- app/src/main/java/com/example/spontaniius/ui/promotions/PromotionTab.kt
+++ app/src/main/java/com/example/spontaniius/ui/promotions/PromotionTab.kt
@@ -3,13 +3,20 @@ package com.example.spontaniius.ui.promotions
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.spontaniius.R
 
 class PromotionTab : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val PromoTitle = "This is a title"
+        val PromoContent = "This is content for a promomotion so that's cool yeetyoatboatmoat beach times kk bye bb"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_promotion_tab)
+        val Title: TextView = findViewById (R.id.Title)
+        Title.setText(PromoTitle)
+        val Content: TextView = findViewById (R.id.Content)
+        Content.setText(PromoContent)
         val button1: Button = findViewById(R.id.BackButton)
         button1.setOnClickListener {
             // Handler code here.

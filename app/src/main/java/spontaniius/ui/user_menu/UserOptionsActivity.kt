9a9644642fb8b65d2fb889d8ca47caae5cd0dcea
Russell <Russell.L.Waterhouse@gmail.com>
2021-05-11 23:04:29 -0700

    Created the skeleton of the User Options Activity

---
+++ app/src/main/java/spontaniius/ui/user_menu/UserOptionsActivity.kt
@@ -0,0 +1,41 @@
+package spontaniius.ui.user_menu
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import spontaniius.R
+
+class UserOptionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_options)
+        val phoneNumberTextView = findViewById<EditText>(R.id.phone_number_edittext)
+        val nameEditText = findViewById<EditText>(R.id.name_edittext)
+        val genderRadioGroup = findViewById<RadioGroup>(R.id.gender_selection_radioGroup)
+
+        val saveButton = this.findViewById<FloatingActionButton>(R.id.save_user_details)
+        val cancelButton = this.findViewById<FloatingActionButton>(R.id.cancel_user_details)
+        saveButton.setOnClickListener {
+            val phoneNumber = phoneNumberTextView.text
+            val name = nameEditText.text
+            val gender = genderRadioGroup.isSelected
+            //TODO: save data here
+        }
+        cancelButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    fun initializeUserData(
+        nameEditText: EditText,
+        phoneNumberEditText: EditText,
+        genderRadioGroup: RadioGroup
+    ) {
+        //TODO: get data and set it to display here
+        nameEditText.setText("previously initialized name")
+        phoneNumberEditText.setText("123-456-7890")
+        genderRadioGroup.check(0)
+    }
+}

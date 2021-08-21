93a86df84165e70276e3f09baee43479767ac6ba
quinnpaterson96 <quinnpaterson1996@gmail.com>
2021-08-20 20:37:08 -0700

    - Added a welcome card

--- app/src/main/java/spontaniius/ui/card_collection/CardCollectionFragment.kt
+++ app/src/main/java/spontaniius/ui/card_collection/CardCollectionFragment.kt
@@ -9,6 +9,7 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import android.widget.AdapterView.GONE
 import android.widget.AdapterView.OnItemClickListener
 import androidx.fragment.app.Fragment
 import com.amplifyframework.auth.options.AuthSignOutOptions
@@ -120,6 +121,11 @@ class CardCollectionFragment : Fragment() {
 
                 val eventJSONArray = JSONArray(response.toString())
                 var currentDate = ""
+
+                if(eventJSONArray.length()==0){
+                    selectedCardName.text="Welcome To Spontaniius"
+                    cardOptionsMenu.visibility= View.GONE
+                }
 
                 for (i in 0 until eventJSONArray.length()) {
                     val event: JSONObject = eventJSONArray.getJSONObject(i)
